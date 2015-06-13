91012be7d80275112104486476a0e3ed9702a2a3
msemik <michal.semik@gmail.com>
2015-06-13 14:12:01 +0200

    nearest neighbours

--- src/main/java/pl/uj/edu/student/tsp/NearestNeighbourTspSolver.java
+++ src/main/java/pl/uj/edu/student/tsp/NearestNeighbourTspSolver.java
@@ -1,9 +1,11 @@
 package pl.uj.edu.student.tsp;
 
+import org.jgrapht.Graphs;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.SimpleWeightedGraph;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Created by michal on 13.06.15.
@@ -11,8 +13,36 @@ import java.util.*;
 public class NearestNeighbourTspSolver implements TspSolver {
     @Override
     public Collection<DefaultWeightedEdge> solve(SimpleWeightedGraph<String, DefaultWeightedEdge> graph) {
-        Set<String> vertexSet = graph.vertexSet();
-        Iterator<String> it = vertexSet.iterator();
-        return Collections.emptyList();
+        List<DefaultWeightedEdge> l = new ArrayList<>();
+        Set<String> visitedVertexes = new HashSet<>();
+
+        String currentVertex = graph.vertexSet().iterator().next();
+        while (true) {
+            Set<DefaultWeightedEdge> edges;
+            edges = graph.edgesOf(currentVertex);
+            edges = removeEdgesContainingVertexes(graph, edges, visitedVertexes);
+
+            if (edges.isEmpty())
+                return l;
+
+            DefaultWeightedEdge edge = minimumWeight(edges, graph);
+            l.add(edge);
+            visitedVertexes.add(currentVertex);
+            currentVertex = Graphs.getOppositeVertex(graph, edge, currentVertex);
+        }
+    }
+
+    private Set<DefaultWeightedEdge> removeEdgesContainingVertexes(SimpleWeightedGraph<String, DefaultWeightedEdge> graph, Set<DefaultWeightedEdge> edges, Set<String> visitedVertexes) {
+        return edges
+                .stream()
+                .filter(edge -> !visitedVertexes.contains(graph.getEdgeSource(edge)) && !visitedVertexes.contains(graph.getEdgeTarget(edge)))
+                .collect(Collectors.toSet());
+    }
+
+    private DefaultWeightedEdge minimumWeight(Set<DefaultWeightedEdge> edges, SimpleWeightedGraph<String, DefaultWeightedEdge> graph) {
+        return edges
+                .stream()
+                .reduce((e1, e2) -> graph.getEdgeWeight(e1) <= graph.getEdgeWeight(e2) ? e1 : e2)
+                .orElse(null);
     }
 }
